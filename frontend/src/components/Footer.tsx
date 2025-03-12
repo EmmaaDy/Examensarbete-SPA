cd5613e9f7dad74872c8dfca8062105adcf4f90c
vickanS <stenvictoria@gmail.com>
2025-03-12 19:11:19 +0100

    footer styling and content

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,37 @@
 import '../styles/Footer.css';
+import Flower from '../assets/Flower.png'
+import leaf from '../assets/leaf.png'; 
+import pattern from '../assets/green-pattern.png'
+import pattern2 from '../assets/green-pattern2.png'
 
-const Footer: React.FC = () => {
+
+const Footer = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} Oceanic Aquarius. All Rights Reserved.</p>
-    </footer>
+  <img src={leaf} alt="Leaf" className="footer-leaf" />
+  <img src={pattern} alt="Pattern" className="footer-pattern pattern-left" />
+
+  <div className="footer-content">
+    <div className='tagline'>
+    <h1>&mdash; hot tub, steam room, sauna, massage, repeat.</h1>
+    </div>
+    <div className="footer-sitemap">
+          <h4>Sitemap</h4>
+          <nav>
+            <a href="/">Home</a>
+            <a href="/about">About Us</a>
+            <a href="/services">Services</a>
+            <a href="/events">Events</a>
+            <a href="/contact">Contact Us</a>
+          </nav>
+        </div>
+  </div>
+
+  <div className="footer-bottom-decorations">
+    <img src={pattern2} alt="Pattern" className="footer-pattern pattern-right" />
+    <img src={Flower} alt="Flower" className="footer-flower" />
+  </div>
+</footer>
   );
 };
 
