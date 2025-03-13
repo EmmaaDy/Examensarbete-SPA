7536f29f901cc0c30985b458c911a3f1eca0f44e
vickanS <stenvictoria@gmail.com>
2025-03-13 09:51:53 +0100

    Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import '../styles/Footer.css';
+import { NavLink } from 'react-router-dom';
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 import Flower from '../assets/Flower.png'
 import leaf from '../assets/leaf.png'; 
 import pattern from '../assets/green-pattern.png'
@@ -13,16 +15,18 @@ const Footer = () => {
 
   <div className="footer-content">
     <div className='tagline'>
-    <h1>&mdash; hot tub, steam room, sauna, massage, repeat.</h1>
+      <h1>&mdash;hot tub, steam room, sauna, massage, repeat.</h1>
     </div>
-    <div className="footer-sitemap">
+        <div className="footer-sitemap">
           <h4>Sitemap</h4>
           <nav>
-            <a href="/">Home</a>
-            <a href="/about">About Us</a>
-            <a href="/services">Services</a>
-            <a href="/events">Events</a>
-            <a href="/contact">Contact Us</a>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/services">Services</NavLink></li>
+            <li><NavLink to="/booking">Booking</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink to="/events">Events</NavLink></li>
+            <li><NavLink to="/admin">Admin Panel</NavLink></li>
           </nav>
         </div>
   </div>
@@ -30,6 +34,19 @@ const Footer = () => {
   <div className="footer-bottom-decorations">
     <img src={pattern2} alt="Pattern" className="footer-pattern pattern-right" />
     <img src={Flower} alt="Flower" className="footer-flower" />
+  </div>
+  <div className='social-icons'>
+    <div className='social-media'>
+      <FaFacebook className="icon" />
+      <FaInstagram className="icon" />
+    </div>
+  </div>
+  <div className='divider'></div>
+  <div className='under-panel'>
+    <p className='copyright'> Copyright Dotcreativemarket</p>
+    <p className='Terms'>Terms of Use</p>
+    <p className='privacy'>Privacy Policy</p>
+    <p className='admin-panel'>Admin panel</p>
   </div>
 </footer>
   );
