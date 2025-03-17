973db00482f05159d89f06a8592c19e094ce2bf2
vickanS <stenvictoria@gmail.com>
2025-03-17 10:14:42 +0100

    some content and styling

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,8 +1,18 @@
+import '../styles/Contact.css'
+
 const Contact = () => {
     return (
-      <div>
-        <h1>Contact Us</h1>
-        <p>If you have any questions, feel free to contact us.</p>
+      <div className='contact'>
+        <section className='contact-header'>
+          <h1>Contact</h1>
+        </section>
+
+        <section className='contact-get-in-touch'>
+          <h2>get in touch!</h2>
+          <p className='p1'>We love hearing from our customers.
+            Whether you have questions about our services, want to book a treatment, or need assistance with planning your next visit,
+            we're here to help!</p>
+        </section>
       </div>
     );
   };
